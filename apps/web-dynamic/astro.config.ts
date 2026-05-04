e3b668c6d39e183aa9b14da06461eee78f9eeda1
jeannegrenet <jeanne.grenet@gmail.com>
2026-05-04 15:35:08 +0200

    fix: astro config for web dynamic

--- apps/web-dynamic/astro.config.ts
+++ apps/web-dynamic/astro.config.ts
@@ -10,7 +10,12 @@ const adapter =
     ? node({ mode: "standalone" })
     : vercel({ isr: true });
 
+const site = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:4321";
+
 export default defineConfig({
+  site,
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
@@ -21,9 +26,8 @@ export default defineConfig({
       ],
     },
     ssr: {
+      external: ["@takumi-rs/core", "@takumi-rs/image-response"],
       noExternal: [
-        "@takumi-rs/image-response",
-        "@takumi-rs/core",
         "@takumi-rs/helpers",
         "@bearstudio/astro-assets-generation",
       ],
